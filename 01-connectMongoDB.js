269641dbd1ccda7f75ce7f9ba5d2e541182db2ae
Rohit Kumar <67726628+rohitverse@users.noreply.github.com>
2022-08-15 16:12:44 +0530

    Delete 01-connectMongoDB.js

--- 01-connectMongoDB.js
+++
@@ -1,9 +0,0 @@
-//Require the Package
-const mongoose = require("mongoose")
-
-//Connection Creation & database Creation
-mongoose.connect("mongodb://localhost:27017/db",
-    { useNewUrlParser: true },
-    { useUnifiedParser: true }
-).then(() =>
-    console.log("Connection successfull....")).catch((err) => { console.log(err) });

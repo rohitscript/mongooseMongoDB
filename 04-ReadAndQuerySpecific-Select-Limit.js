95e4ebb1bc2d464a0e68949c2e8114a7f8efde43
Rohit Kumar <67726628+rohitverse@users.noreply.github.com>
2022-08-15 12:00:39 +0530

    Add files via upload

---
+++ 04-ReadAndQuerySpecific-Select-Limit.js
@@ -0,0 +1,52 @@
+//Require the Package
+const mongoose = require("mongoose")
+
+//Connection Creation & database Creation
+mongoose.connect("mongodb://localhost:27017/db",
+    { useNewUrlParser: true },
+    { useUnifiedParser: true }
+).then(() =>
+    console.log("Connection successfull....")).catch((err) => { console.log(err) });
+
+//Schema Define
+//A Mongoose Schema defines the structure of the document,default value,validators etc.
+const playlistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+//Mongoose Model 
+//A Mongoose model is a Wrapper on the Mongoose schema.
+
+//A Mongoose Schema defines the structure of the document,default value,validators etc.
+//whereas, a mongoose model provides an interface to the database for creating, queryng,updating deleting records etc.
+
+
+//Collection Creation
+const Playlist = new mongoose.model("Playlist", playlistSchema);
+
+//Read or Querying Documents using field:value ,select & limt
+
+
+const getDocument = async () => {
+    try {
+        const result = await Playlist.find(
+            {ctype:"frontend"}).select({name:1}).limit(1)
+        console.log(result)
+    } catch (err) {
+        Console.log(err);
+    }
+}
+getDocument();
+
